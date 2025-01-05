b1f97f9f871de7caba24d8e46f5cd4d9c4dca201
ABC <mayukc55@gmail.com>
2025-01-05 12:51:08 +0530

    day 1

---
+++ JS/DAY1/first.js
@@ -0,0 +1,26 @@
+console.log("hello world 2.O");
+
+console.log(23+45);
+console.log(45.01==(22+23.01)); // false
+console.log(12.345+89.7);
+
+// == and ===
+
+
+console.log('5'==5); // it will automatically type cast the expression (compare the value only)
+console.log('5'===5); // it will check the type as well (compare the value and type)
+
+
+console.log("0"==0); //true
+console.log('0'===0); // false
+
+console.log(false==0); // true
+console.log(true==="1"); // false
+console.log(true=="1"); // true
+console.log(true==false); // false
+
+
+
+console.log('5'+2);
+console.log(typeof(Number('3.1234')));
+console.log(eval('2*4+6')); // it evaluates the mathematical expression inside of a string
